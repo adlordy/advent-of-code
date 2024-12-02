64c432b1ffb8916a75ffbeeef4df271c839a7115
Bulat Gafurov <adlordy@gmail.com>
2024-12-02 22:02:57 +0000

    Day 1 and 2

---
+++ 2024/day-1/Program.cs
@@ -0,0 +1,28 @@
+﻿var content = await core.AdventOfCode.GetContent(1);
+var lines = content.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+var a = new int[lines.Length];
+var b = new int[lines.Length];
+
+for(var i=0;i<lines.Length;i++){
+    var values = lines[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    a[i] = int.Parse(values[0]);
+    b[i] = int.Parse(values[1]);
+}
+
+int Promblem1(){
+    Array.Sort(a);
+    Array.Sort(b);
+    var sum = 0;
+
+    for(var i=0;i<a.Length;i++){
+        sum += Math.Abs(a[i] - b[i]);
+    }
+    return sum;
+}
+
+int Problem2(){
+    var c = b.GroupBy(x => x).ToDictionary(x => x.Key, g => g.Count());
+    return a.Select(x => c.TryGetValue(x, out var d) ? x*d : 0).Sum();
+}
+
+Console.WriteLine(Problem2());
---
+++ 2024/day-2/Program.cs
@@ -0,0 +1,60 @@
+﻿var content = await core.AdventOfCode.GetContent(2);
+var lines = content.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+bool Check(IEnumerable<int> numbers){
+    var e = numbers.GetEnumerator();
+    e.MoveNext();
+    var prev = e.Current;
+    var dir = 0;
+    while(e.MoveNext()){
+        var current = e.Current;
+        var diff = current - prev;
+        var absDiff = Math.Abs(diff);
+        if (dir == 0)
+            dir = Math.Sign(diff);
+        if (absDiff < 1 || absDiff > 3 || dir != Math.Sign(diff))
+            return false;
+        prev = current;
+    }
+    return true;
+}
+
+bool Check2(int[] numbers){
+    if (Check(numbers)){
+        return true;
+    }
+    else 
+    {
+        for(var i=0;i<numbers.Length;i++){
+            int[] variation = [..numbers[..i],..numbers[(i+1)..]];
+            if (Check(variation))
+                return true;
+        }
+    }
+    return false;
+}
+
+int Problem1(){
+    var count = 0;
+    foreach(var line in lines){
+        var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        var numbers = parts.Select(int.Parse);
+        if (Check(numbers)){
+            count++;
+        }
+    }
+    return count;
+}
+
+int Problem2(){
+    var count = 0;
+    foreach(var line in lines){
+        var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        var numbers = parts.Select(int.Parse).ToArray();
+        if (Check2(numbers)){
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(Problem2());
